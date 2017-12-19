bf7fffdd83bc69f88a03942d0f004407214fc637
li.liu <li.liu@china.zhaogang.com>
2017-12-19 18:26:07 +0800

    redis

--- src/main/java/net/mrliuli/App.java
+++ src/main/java/net/mrliuli/App.java
@@ -1,5 +1,10 @@
 package net.mrliuli;
 
+import redis.clients.jedis.*;
+
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,26 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        Jedis jedis = new Jedis("localhost");
+
+        System.out.println(jedis.auth("leon"));
+
+        System.out.println(jedis.ping());
+
+        System.out.println(jedis.get("myKey"));
+        System.out.println(jedis.get("book-name"));
+
+        System.out.println(jedis.set("runoobkey", "www.runoob.com"));
+        System.out.println(jedis.get("runoobkey"));
+
         System.out.println( "Hello World!" );
+
+        Set<String> keys = jedis.keys("*");
+        Iterator<String> it = keys.iterator();
+        while(it.hasNext()){
+            String key = it.next();
+            System.out.println(key);
+        }
     }
 }
